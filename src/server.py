cbb857d78c29ebb773b43795dc30aa9ef029a88a
Tomaz Strukelj <cofkomail@gmail.com>
2024-08-11 10:51:16 +0200

    fixed help message

--- src/server.py
+++ src/server.py
@@ -787,7 +787,7 @@ def parse_args():
     parser.add_argument('-c', '--config', type=str, default='config.yaml', help='Path to the configuration file. If omitted, `config.yaml` in the current directory is used by default')
     parser.add_argument('--http-port', type=int, default=8080, help='Port to run the HTTP server on (default: 8080)')
     parser.add_argument('--https-port', type=int, default=8443, help='Port to run the HTTPS server on (default: 8443)')
-    parser.add_argument('--cert', type=str, help='Path to the TLS certificate file. This can be server certificate alone, or a bundle of (1) server, (2) intermediary and (3) root CA certificate, in this order, like TLS expects it.')
+    parser.add_argument('--cert', type=str, help='Path to the TLS certificate file. This can be server certificate alone, or a bundle of (1) server, (2) intermediary and (3) root CA certificate, in this order, like TLS expects it')
     parser.add_argument('--key', type=str, help='Path to the TLS key file')
     parser.add_argument('--firewall-type', type=str, default='iptables', choices=['iptables', 'nftables', 'vyos'], help='Type of routing to use (default: iptables)')
     parser.add_argument('--nftables-table-filter', type=str, help='filter table to be used for filter chains (vyos: defaults to vyos_filter)')
@@ -799,7 +799,7 @@ def parse_args():
     parser.add_argument('--nftables-chain-default-forward', type=str, help='chain hooked to forward, used for non-local services access')
     parser.add_argument('--nftables-chain-default-prerouting', type=str, help='chain hooked to nat prerouting, used for non-local services access')
     parser.add_argument('--nftables-chain-default-postrouting', type=str, help='chain hooked to nat postrouting, used for non-local services access')
-    parser.add_argument('--service-rule-cleanup-on-shutdown', action='store_false', default=True, help='Cleanup also firewall service(s) rules on shutdown in addition to those for management (HTTP/HTTPS). Set this if you want to keep access to services when KnockPort is shut down')
+    parser.add_argument('--service-rule-cleanup-on-shutdown', action='store_false', default=True, help='Drop access also to services ports in addition to management (HTTP/HTTPS) ports when KnockPort is shut down. Default is to keep service port rules as is to not disrupt the services access when restarting KnockPort')
     args = parser.parse_args()
     return args
 
